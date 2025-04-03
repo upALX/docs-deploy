15282a122f77be3816604472a6ba5e8a2cd6722e
José Alexandre <alexandreaddres@gmail.com>
2025-04-02 22:19:25 -0300

    adding first docstring tested with doctest tool

--- calculator/calculations.py
+++ calculator/calculations.py
@@ -1,7 +1,21 @@
 # calculator/calculations.py
 
 def add(a, b) -> float:
+    """
+    function to sum two float numbers.
 
+        Examples:
+            >>> add(4.0, 2.0)
+            6.0
+            >>> add(4, 2)
+            6.0
+
+        Args:
+            a (float): first number to sum.
+            b (float):  second number to sum.
+        Returns:
+            float: A number representing the arithmetic sum of `a` and `b`.     
+    """
     return float(a + b)
 
 def subtract(a, b) -> float:
